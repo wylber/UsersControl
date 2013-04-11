2ed89268317e11694f4b6dfd9e301a8c058df618
wylber <wylber_eu@hotmail.com>
2013-04-11 19:57:31 -0400

    prog inicial

---
+++ Cadastro.java
@@ -0,0 +1,31 @@
+
+public class Cadastro1 {
+ public static void main(String[]args){
+ 
+ 
+  int resp=0;
+  
+  while (resp != 999){
+  System.out.println ("     Digite o comando a ser Usado     ");
+  Sytem.out.println ("     1- adicionr User           ");
+  System.out.println("     2- Mostrar os User Cadastrados    ");
+  System.out.println("     3- Remover User          ");
+  System.out.println("     4-Exportar Cadastro      ");
+  System.out.prinln("   5-Importar Cadastro      ");
+  
+  if (resp > 5){
+  System.out.printl("Comando Invalido,Digite Outro Comando ");
+  }
+  
+  }
+}
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    
---
+++ User.java
@@ -0,0 +1,16 @@
+public class User{
+
+	String nome;
+	String email;
+	int id;
+	
+	
+	public int Add( String nome, email){
+		users u = new Users();
+		u.add("nome");
+		u.add("email");
+		u.add("id");
+	}	
+}
+	
+		
