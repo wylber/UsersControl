54d39b717089065f05a3f6b254f2b3ae8e119d65
lenoncabral <lenon.cabral@hotmail.com>
2013-04-11 21:33:31 -0300

    modificao no users e no cadastro

--- Cadastro.java
+++ Cadastro.java
@@ -2,30 +2,25 @@
 public class Cadastro1 {
  public static void main(String[]args){
  
- 
   int resp=0;
   
   while (resp != 999){
-  System.out.println ("     Digite o comando a ser Usado     ");
-  Sytem.out.println ("     1- adicionr User           ");
+  System.out.println ("    Digite o comando a ser Usado     ");
+  System.out.println ("    1- adicionr User           ");
   System.out.println("     2- Mostrar os User Cadastrados    ");
   System.out.println("     3- Remover User          ");
   System.out.println("     4-Exportar Cadastro      ");
-  System.out.prinln("   5-Importar Cadastro      ");
+  System.out.prinln("      5-Importar Cadastro      ");
   
   if (resp > 5){
   System.out.printl("Comando Invalido,Digite Outro Comando ");
+
+  if (resp = 1) {
+    User User = new User();
+    User.nome ;
+    User.email ;
+    User.id = id++;
   }
-  
+  }
   }
 }
-}
- 
- 
- 
- 
- 
- 
- 
- 
-    
--- User.java
+++ User.java
@@ -2,15 +2,13 @@ public class User{
 
 	String nome;
 	String email;
-	int id;
-	
-	
-	public int Add( String nome, email){
+	int id=0;
+
+
+	public void Add( String nome, email){
 		users u = new Users();
 		u.add("nome");
 		u.add("email");
 		u.add("id");
 	}	
 }
-	
-		
